e30dffb08fe231fbe453ee9674f041caf5271249
tbelk12 <taylor.belk@americannational.com>
2019-07-31 11:15:10 -0500

    added switch to routes for netlify support

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './Home/mainHome';
 import StudentDashboard from './Student/Dashboard/student-dashboard-main';
 import CourseListMain from './CourseRegistration/courselist';
@@ -10,11 +10,13 @@ import StudentLogin from './Login/StudentLogin/studentLogin';
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/student/signup" component={StudentSignUp} />
-      <Route exact path="/student/login" component={StudentLogin} />
-      <Route exact path="/student/dashboard" component={StudentDashboard} />
-      <Route exact path="/register/courses" component={CourseListMain} />
+      <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/student/signup" component={StudentSignUp} />
+          <Route exact path="/student/login" component={StudentLogin} />
+          <Route exact path="/student/dashboard" component={StudentDashboard} />
+          <Route exact path="/register/courses" component={CourseListMain} />
+      </Switch>
     </div>
   );
 }
